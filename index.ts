7d276cc4a70f7b02659d1756d463e52bb64c2260
Maria678901 <mariakhan061022@gmail.com>
2024-04-03 13:54:10 -0700

    push my cli guessing game on github

---
+++ index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber=Math.floor(Math.random()*6+1);
+
+
+const answer =await inquirer.prompt([
+    {
+        name:"userGuesssedNumber",
+        type:"number",
+        message:"please guess a number between 1-6",
+    },
+]);
+if (answer.userGuesssedNumber===randomNumber){
+    console.log("congratulations you guessed right number");
+}
+else{
+    console.log("you guessed wrong number");
+}
